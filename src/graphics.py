a3379ba1ec7ac97f36f653dd51efd5a9218b786c
Pedromrls <pedromrls6@gmail.com>
2025-02-17 13:12:29 -0500

    Minor changes at the organization of the cell class

--- src/graphics.py
+++ src/graphics.py
@@ -19,7 +19,7 @@ class Window:
         while self.__running:
             self.redraw()
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color=fill_color)
 
     def close(self):
@@ -51,10 +51,6 @@ class Line:
 class Cell:
     def __init__(
         self,
-        x1,
-        y1,
-        x2,
-        y2,
         win,
         has_left_wall=True,
         has_right_wall=True,
@@ -65,29 +61,33 @@ class Cell:
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
         self.has_bottom_wall = has_bottom_wall
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
-        self._win = win
-
-    def draw(self):
-        top_left = Point(self._x1, self._y1)
-        top_right = Point(self._x2, self._y1)
-        bottom_left = Point(self._x1, self._y2)
-        bottom_right = Point(self._x2, self._y2)
+        __top_left = Point(x1, y1)
+        __top_right = Point(x2, y1)
+        __bottom_left = Point(x1, y2)
+        __bottom_right = Point(x2, y2)
         if self.has_left_wall:
-            left_wall = Line(top_left, bottom_left)
-            self._win.draw_line(left_wall, "white")
+            left_wall = Line(__top_left, __bottom_left)
+            self._win.draw_line(left_wall)
 
         if self.has_right_wall:
-            right_wall = Line(top_right, bottom_right)
-            self._win.draw_line(right_wall, "white")
+            right_wall = Line(__top_right, __bottom_right)
+            self._win.draw_line(right_wall)
 
         if self.has_top_wall:
-            top_wall = Line(top_left, top_right)
-            self._win.draw_line(top_wall, "white")
+            top_wall = Line(__top_left, __top_right)
+            self._win.draw_line(top_wall)
 
         if self.has_bottom_wall:
-            bottom_wall = Line(bottom_left, bottom_right)
-            self._win.draw_line(bottom_wall, "white")
+            bottom_wall = Line(__bottom_left, __bottom_right)
+            self._win.draw_line(bottom_wall)
--- src/main.py
+++ src/main.py
@@ -6,16 +6,16 @@ def main():
     win = Window(800, 600)
 
     # Test 1: Default cell (all walls)
-    cell1 = Cell(50, 50, 100, 100, win)
-    cell1.draw()
+    cell1 = Cell(win)
+    cell1.draw(50, 50, 100, 100)
 
     # Test 2: Cell with no right wall
-    cell2 = Cell(150, 50, 200, 100, win, has_right_wall=False)
-    cell2.draw()
+    cell2 = Cell(win, has_right_wall=False)
+    cell2.draw(150, 50, 200, 100)
 
     # Test 3: Cell with only top and bottom walls
-    cell3 = Cell(250, 50, 300, 100, win, has_left_wall=False, has_right_wall=False)
-    cell3.draw()
+    cell3 = Cell(win, has_left_wall=False, has_right_wall=False)
+    cell3.draw(250, 50, 300, 100)
 
     win.wait_for_close()
 
